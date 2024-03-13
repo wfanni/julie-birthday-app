89b139d072f3d18caf2d0c318721acba7f64b1b2
Fanni Wihl <fanniwihl@gmail.com>
2024-03-13 22:46:50 +0100

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.PNG'],
+  build: {
+    outDir: 'build',
+  },
+  base: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  }
 })
