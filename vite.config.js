7d4b2c08adf78e81e6ccfc810b3cb16388456363
Fanni Wihl <fanniwihl@gmail.com>
2024-03-14 11:59:59 +0100

    path updates

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.PNG'],
+  assetsInclude: ['**/*.PNG', "**/*.JPG"],
   build: {
     outDir: 'build',
   },
